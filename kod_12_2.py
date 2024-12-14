68693613d1dd87f55f0118057e101eff8598331d
Лена <trylya@mail.ru>
2024-12-14 13:10:37 +0300

    Юнит-тестирование

--- kod_12_2.py
+++ kod_12_2.py
@@ -7,41 +7,33 @@ class Runner:
     def run(self):
         self.distance += self.speed * 2
 
-    class Runner:
-        def __init__(self, name, speed=5):
-            self.name = name
-            self.distance = 0
-            self.speed = speed
+    def walk(self):
+        self.distance += self.speed
 
-        def run(self):
-            self.distance += self.speed * 2
+    def __str__(self):
+        return self.name
 
-        def walk(self):
-            self.distance += self.speed
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
 
-        def __str__(self):
-            return self.name
 
-        def __eq__(self, other):
-            if isinstance(other, str):
-                return self.name == other
-            elif isinstance(other, Runner):
-                return self.name == other.name
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
 
-    class Tournament:
-        def __init__(self, distance, *participants):
-            self.full_distance = distance
-            self.participants = list(participants)
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
 
-        def start(self):
-            finishers = {}
-            place = 1
-            while self.participants:
-                for participant in self.participants:
-                    participant.run()
-                    if participant.distance >= self.full_distance:
-                        finishers[place] = participant
-                        place += 1
-                        self.participants.remove(participant)
-
-            return finishers
+        return finishers
--- tests_12_2.py
+++ tests_12_2.py
@@ -1,50 +1,53 @@
+import kod_12_2
 import unittest
-import inspect
-import Runner, Tournament
 
 
 class TournamentTest(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
     def setUp(self):
-        self.usain = Runner('Усэйн', 10)
-        self.andrey = Runner('Андрей', 9)
-        self.nik = Runner('Ник', 3)
+        self.runer_1 = kod_12_2.Runner('Усэйн', 10)
+        self.runer_2 = kod_12_2.Runner('Андрей', 9)
+        self.runer_3 = kod_12_2.Runner('Ник', 3)
 
     @classmethod
     def tearDownClass(cls):
-        print()
-        for test in cls.all_results:
-            print()
-            print(f'{test}:')
-            print({k: str(v) for k, v in cls.all_results[test].items()})
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
 
-    def test_usain_nik(self):
-        tour = Tournament(90, self.usain, self.nik)
-        results = tour.start()
-        self.all_results[inspect.stack()[0][3]] = results
-        self.assertTrue('Ник' == results[len(results)].name)
+    def test_turn1(self):
+        # list_test = [[self.runer_1, self.runer_3], [self.runer_2, self.runer_3],
+        #              [self.runer_1, self.runer_2, self.runer_3]]
+        turn_1 = kod_12_2.Tournament(90, self.runer_1, self.runer_3)
+        result = turn_1.start()
+        # print(result[list(result.keys())[-1]] == 'Ник')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn1'] = result
 
-    def test_andrey_nik(self):
-        tour = Tournament(90, self.andrey, self.nik)
-        results = tour.start()
-        self.all_results[inspect.stack()[0][3]] = results
-        self.assertTrue('Ник' == results[len(results)].name)
+    def test_turn2(self):
+        turn_2 = kod_12_2.Tournament(90, self.runer_2, self.runer_3)
+        result = turn_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn2'] = result
 
-    def test_usain_andrey_nik(self):
-        tour = Tournament(90, self.usain, self.andrey, self.nik)
-        results = tour.start()
-        self.all_results[inspect.stack()[0][3]] = results
-        self.assertTrue('Ник' == results[len(results)].name)
+    def test_turn3(self):
+        turn_3 = kod_12_2.Tournament(90, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn3'] = result
 
-    def test_nik_andrey_usain_8(self):
-        tour = Tournament(8, self.nik, self.andrey, self.usain)
-        results = tour.start()
-        self.all_results[inspect.stack()[0][3]] = results
-        self.assertTrue('Ник' == results[len(results)].name)
+    def test_turn4(self):
+
+        turn_4 = kod_12_2.Tournament(6, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_4.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn4'] = result
 
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
